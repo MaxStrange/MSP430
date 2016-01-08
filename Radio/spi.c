0e16cd0ff2b60f8ef3fb587b46420b4f3aceec1c
Max Strange <stranm1@spu.edu>
2016-01-08 15:07:16 -0800

    Reverted to not using circular queue

--- Radio/spi.c
+++ Radio/spi.c
@@ -1,47 +1,6 @@
 #include <msp430.h>
 #include "spi.h"
 
-//This version works in loopback mode
-//void spi_init(void)
-//{
-//	/*
-//	 * Initialize pin configurations
-//	 *
-//	 * See p.76 of device-specific datasheet
-//	 */
-//	P1SEL1 |= BIT6 | BIT7;				//pins P1.6 and P1.7 are MOSI and MISO respectively
-//	P1SEL0 &= ~(BIT6 | BIT7);
-//
-//	P1SEL1 |= BIT3;						//pin P1.3 is STE
-//	P1SEL0 &= ~BIT3;
-//
-//	P2SEL1 |= BIT2;						//pin P2.2 is CLK
-//	P2SEL0 &= ~BIT2;
-//
-//
-//	/*
-//	 * Initialize the SPI module.
-//	 *
-//	 * 1. Enable software reset.
-//	 * 2. Configure SPI
-//	 * 3. Clear sw reset, resume operation
-//	 * 4. Enable RX interrupt
-//	 */
-//
-//	UCB0CTLW0 |= UCSWRST;							//software reset
-//
-//	UCB0CTLW0 |= UCMSB | UCMST | UCSYNC | UCCKPL;	//Inactive high, synchronous, master, msb first
-//	UCB0CTLW0 |= UCSSEL_2;							//use SMCLK
-//	UCB0BR0 |= 0x02;
-//	UCB0BR1 = 0;									//configure clock freq
-//	UCB0STATW |= UCLISTEN;							//enable loopback mode
-//
-//	UCB0CTLW0 &= ~UCSWRST;							//disable software reset, enable operation
-//
-//	UCB0IE |= UCRXIE;								//enable rx interrupt
-//}
-
-
 void spi_init(void)
 {
 	/*
@@ -49,14 +8,16 @@ void spi_init(void)
 	 *
 	 * See p.76 of device-specific datasheet
 	 */
-	P1SEL1 |= BIT6 | BIT7;				//pins P1.6 and P1.7 are MOSI and MISO respectively
-	P1SEL0 &= ~(BIT6 | BIT7);
+	P1SEL1 |= MOSI | MISO;				//pins P1.6 and P1.7 are MOSI and MISO respectively
+	P1SEL0 &= ~(MOSI | MISO);
 
-	P1SEL1 |= BIT3;						//pin P1.3 is STE
-	P1SEL0 &= ~BIT3;
+//	P1SEL1 |= CSN;						//pin P1.3 is STE
+//	P1SEL0 &= ~CSN;
+	P1DIR |= CSN;
+	P1OUT |= CSN;
 
-	P2SEL1 |= BIT2;						//pin P2.2 is CLK
-	P2SEL0 &= ~BIT2;
+	P2SEL1 |= CLK;						//pin P2.2 is CLK
+	P2SEL0 &= ~CLK;
 
 
 	/*
@@ -71,7 +32,7 @@ void spi_init(void)
 	UCB0CTLW0 |= UCSWRST;							//software reset
 
 	UCB0CTLW0 |= UCMSB | UCMST | UCSYNC;			//Inactive low, synchronous, master, msb first
-	UCB0CTLW0 |= UCSSEL_2 | UCMODE_2 | UCSTEM;		//use SMCLK, 4 pin mode with active low on CSN (STE)
+	UCB0CTLW0 |= UCSSEL_2;//SMCLK 3-pin mode | UCMODE_2 | UCSTEM;		//use SMCLK, 4 pin mode with active low on CSN (STE)
 	UCB0BR0 |= 0x02;
 	UCB0BR1 = 0;									//configure clock freq
 	UCB0STATW |= UCLISTEN;							//enable loopback mode
--- Radio/spi.h
+++ Radio/spi.h
@@ -1,6 +1,10 @@
 #ifndef SPI_H_
 #define SPI_H_
 
+#define MOSI	BIT6
+#define MISO	BIT7
+#define CSN		BIT3
+#define CLK		BIT2
 
 void spi_init(void);
 
